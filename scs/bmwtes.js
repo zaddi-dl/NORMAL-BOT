4cc748c0bb91b37b6e393cd88d440733dcce734b
ibraah tech <munjueko@gmail.com>
2024-09-28 15:54:23 +0300

    Update bmwtes.js

--- scs/bmwtes.js
+++ scs/bmwtes.js
@@ -26,11 +26,11 @@ adams({
       const videoUrl = videos[0].url;
 
       // https://abu-2f6def455bb4.herokuapp.com/download/ytmp3?url=https://music.youtube.com/watch?v=6POZlJAZsok&si=FgIl9XCg__1V8m4_}`);
-      const apiResponse = await fetch(`https://abu-2f6def455bb4.herokuapp.com/ytmp4?url=${encodeURIComponent(Element.url)}`);
+      const apiResponse = await fetch(`https://abu-2f6def455bb4.herokuapp.com/download/ytmp4?url=${encodeURIComponent(videoUrl)}`);
       const apiResult = await apiResponse.json();
 
       if (apiResult.status === 'success ✅') {
-        const videoUrl = apiResult.data.download;
+        const videoDlUrl = apiResult.data.download;
         const fileInfo = {
           title: apiResult.data.title,
           fileSize: apiResult.data.file_size,

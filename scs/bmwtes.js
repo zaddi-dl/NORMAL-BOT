accb9366681650df959cc2303f02ed78ad7ddca4
ibraah tech <munjueko@gmail.com>
2024-09-28 15:15:06 +0300

    Update bmwtes.js

--- scs/bmwtes.js
+++ scs/bmwtes.js
@@ -1,12 +1,70 @@
-const _0x13586c=_0x5488;function _0x15aa(){const _0x43da60=['6OvAvup','video','timestamp','Searching...⏳','2149jDYQUk','Error\x20from\x20API:','Please\x20insert\x20a\x20song/video\x20name.','7112UTJSse','length','\x20by\x20','*BMW\x20SONG\x20PLAYER*\x0a\x0a*◁\x20II\x20▷\x201:00\x20•၊၊||၊|။||။||၊|||။\x20၊|•','45065OYDLVu','An\x20error\x20occurred\x20while\x20searching\x20or\x20downloading\x20the\x20audio.','thumbnail','/api/download/ytmp3?url=','517308BoMcIN','59934yvyjHj','audio/mp4','90ygBqol','134387mrLuZl','../Ibrahim/adams','Please\x20insert\x20a\x20song\x20name.','url','55eUxlrj','https://abu-2f6def455bb4.herokuapp.com','*\x0a\x0a*©Ibrahim\x20Adams*','json','status','videos','play','yt-search','success','download_url','result','*Bmw\x20Just\x20Downloaded\x20','join','video/mp4','error','song','2789118GBaBVM','Failed\x20to\x20download\x20audio.\x20Please\x20try\x20again\x20later.','title','*\x0a\x0a*®Adams\x202024*','356463sCqOYg','sendMessage'];_0x15aa=function(){return _0x43da60;};return _0x15aa();}(function(_0x571e94,_0x3626a8){const _0x4901b7=_0x5488,_0x114bac=_0x571e94();while(!![]){try{const _0x5e932e=parseInt(_0x4901b7(0x145))/0x1+-parseInt(_0x4901b7(0x13a))/0x2*(parseInt(_0x4901b7(0x138))/0x3)+parseInt(_0x4901b7(0x149))/0x4+-parseInt(_0x4901b7(0x151))/0x5*(parseInt(_0x4901b7(0x14a))/0x6)+parseInt(_0x4901b7(0x13e))/0x7*(parseInt(_0x4901b7(0x141))/0x8)+parseInt(_0x4901b7(0x134))/0x9+parseInt(_0x4901b7(0x14c))/0xa*(-parseInt(_0x4901b7(0x14d))/0xb);if(_0x5e932e===_0x3626a8)break;else _0x114bac['push'](_0x114bac['shift']());}catch(_0x498f33){_0x114bac['push'](_0x114bac['shift']());}}}(_0x15aa,0x2c2ba));const {adams}=require(_0x13586c(0x14e)),yts=require(_0x13586c(0x12b)),BaseUrl=_0x13586c(0x125);function _0x5488(_0x109f2c,_0x3e158c){const _0x15aa07=_0x15aa();return _0x5488=function(_0x5488ac,_0x432972){_0x5488ac=_0x5488ac-0x125;let _0x5bda6b=_0x15aa07[_0x5488ac];return _0x5bda6b;},_0x5488(_0x109f2c,_0x3e158c);}adams({'nomCom':_0x13586c(0x13b),'categorie':'Search','reaction':'🎥'},async(_0x1720fe,_0x55cdf5,_0x55ec42)=>{const _0x1dceee=_0x13586c,{ms:_0x432e6b,repondre:_0x296ddc,arg:_0x39b839}=_0x55ec42;if(!_0x39b839[0x0]){_0x296ddc(_0x1dceee(0x140));return;}try{let _0x2ec2c4=_0x39b839[_0x1dceee(0x130)]('\x20'),_0x1cd040=[];const _0x39cb24=await yts(_0x2ec2c4);_0x1cd040=_0x39cb24[_0x1dceee(0x129)];if(_0x1cd040&&_0x1cd040['length']>0x0){const _0x341c45=_0x1cd040[0x0][_0x1dceee(0x150)],_0x2fb027=await fetch(BaseUrl+'/api/download/ytmp4?url='+encodeURIComponent(_0x341c45)),_0x283053=await _0x2fb027[_0x1dceee(0x127)]();if(_0x283053['status']===0xc8&&_0x283053[_0x1dceee(0x12c)]){const _0x5b0b5a=_0x283053[_0x1dceee(0x12e)]['download_url'],_0xda4375={'image':{'url':_0x1cd040[0x0][_0x1dceee(0x147)]},'caption':'Bmw\x20is\x20downloading\x20'+_0x283053[_0x1dceee(0x12e)][_0x1dceee(0x136)]+_0x1dceee(0x143)+_0x1cd040[0x0]['author']['name']+'\x0a\x20Time\x20:\x20'+_0x1cd040[0x0][_0x1dceee(0x13c)]+'\x0a\x0a\x0a>\x20©Ibrahim\x20Adams'};await _0x55cdf5[_0x1dceee(0x139)](_0x1720fe,_0xda4375,{'quoted':_0x432e6b}),await _0x55cdf5[_0x1dceee(0x139)](_0x1720fe,{'video':{'url':_0x5b0b5a},'mimetype':_0x1dceee(0x131)},{'quoted':_0x432e6b}),_0x296ddc('Downloded\x20Successfully\x20✅');}else _0x296ddc(_0x1dceee(0x13d));}else _0x296ddc('No\x20videos\x20found.');}catch(_0x4c47bd){console[_0x1dceee(0x132)](_0x1dceee(0x13f),_0x4c47bd),_0x296ddc('Searching...⏳');}}),adams({'nomCom':_0x13586c(0x12a),'categorie':'Download','reaction':'🎧'},async(_0x5b126d,_0x1fb4b8,_0x238e59)=>{const _0x4a77db=_0x13586c,{ms:_0x2793f1,repondre:_0x479aa1,arg:_0x47b224}=_0x238e59;if(!_0x47b224[0x0]){_0x479aa1(_0x4a77db(0x14f));return;}try{let _0x1b4221=_0x47b224[_0x4a77db(0x130)]('\x20'),_0x50cf21=[];const _0x482303=await yts(_0x1b4221);_0x50cf21=_0x482303['videos'];if(_0x50cf21&&_0x50cf21[_0x4a77db(0x142)]>0x0){const _0x49b272=_0x50cf21[0x0][_0x4a77db(0x150)],_0x13e3e5=await fetch(BaseUrl+_0x4a77db(0x148)+encodeURIComponent(_0x49b272)),_0x405ac7=await _0x13e3e5[_0x4a77db(0x127)]();if(_0x405ac7[_0x4a77db(0x128)]===0xc8&&_0x405ac7[_0x4a77db(0x12c)]){const _0x875c28=_0x405ac7[_0x4a77db(0x12e)][_0x4a77db(0x12d)],_0x2b58a5={'image':{'url':_0x50cf21[0x0][_0x4a77db(0x147)]},'caption':_0x4a77db(0x144)+_0x50cf21[0x0][_0x4a77db(0x13c)]+_0x4a77db(0x126)};await _0x1fb4b8[_0x4a77db(0x139)](_0x5b126d,_0x2b58a5,{'quoted':_0x2793f1}),await _0x1fb4b8[_0x4a77db(0x139)](_0x5b126d,{'audio':{'url':_0x875c28},'mimetype':_0x4a77db(0x14b)},{'quoted':_0x2793f1}),_0x479aa1(_0x4a77db(0x12f)+_0x405ac7['result'][_0x4a77db(0x136)]+'*\x0a\x0a*®Adams\x202024*');}else _0x479aa1(_0x4a77db(0x135));}else _0x479aa1('No\x20audio\x20found.');}catch(_0x3df847){console[_0x4a77db(0x132)](_0x4a77db(0x13f),_0x3df847),_0x479aa1(_0x4a77db(0x146));}}),adams({'nomCom':_0x13586c(0x133),'categorie':'Download','reaction':'🎸'},async(_0x479446,_0x46e7e8,_0x3741fc)=>{const _0x3ebb3a=_0x13586c,{ms:_0x3ee4a1,repondre:_0x50b0cc,arg:_0x85a00c}=_0x3741fc;if(!_0x85a00c[0x0]){_0x50b0cc('Please\x20insert\x20a\x20song\x20name.');return;}try{let _0x58c48a=_0x85a00c[_0x3ebb3a(0x130)]('\x20'),_0x2a815f=[];const _0x1a433a=await yts(_0x58c48a);_0x2a815f=_0x1a433a[_0x3ebb3a(0x129)];if(_0x2a815f&&_0x2a815f[_0x3ebb3a(0x142)]>0x0){const _0x564569=_0x2a815f[0x0]['url'],_0x3f480b=await fetch(BaseUrl+_0x3ebb3a(0x148)+encodeURIComponent(_0x564569)),_0x4b07d9=await _0x3f480b['json']();if(_0x4b07d9[_0x3ebb3a(0x128)]===0xc8&&_0x4b07d9['success']){const _0x2a9c60=_0x4b07d9[_0x3ebb3a(0x12e)]['download_url'],_0x44a40c={'image':{'url':_0x2a815f[0x0][_0x3ebb3a(0x147)]},'caption':_0x3ebb3a(0x144)+_0x2a815f[0x0]['timestamp']+'*\x0a\x0a*©Ibrahim\x20Adams*'};await _0x46e7e8[_0x3ebb3a(0x139)](_0x479446,_0x44a40c,{'quoted':_0x3ee4a1}),await _0x46e7e8[_0x3ebb3a(0x139)](_0x479446,{'audio':{'url':_0x2a9c60},'mimetype':_0x3ebb3a(0x14b)},{'quoted':_0x3ee4a1}),_0x50b0cc('*Bmw\x20Just\x20Downloaded\x20'+_0x4b07d9[_0x3ebb3a(0x12e)]['title']+_0x3ebb3a(0x137));}else _0x50b0cc(_0x3ebb3a(0x135));}else _0x50b0cc('No\x20audio\x20found.');}catch(_0x51fe2a){console[_0x3ebb3a(0x132)](_0x3ebb3a(0x13f),_0x51fe2a),_0x50b0cc(_0x3ebb3a(0x146));}});
-/**
+
 const { adams } = require("../Ibrahim/adams");
 const yts = require('yt-search');
-//const adams = 'giftedtechk';
-//const adams = 'prabath-api_5f6557';
+const BaseUrl = 'https://abu-2f6def455bb4.herokuapp.com';
+adams({
+  nomCom: "video",
+  categorie: "Search",
+  reaction: "🎥"
+}, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("Please insert a song/video name.");
+    return;
+  }
+
+  try {
+    let topo = arg.join(" ");
+    let videos = [];
+
+    // Perform YouTube search
+    const search = await yts(topo);
+    videos = search.videos;
+
+    if (videos && videos.length > 0) {
+      const videoUrl = videos[0].url;
+
+      // https://abu-2f6def455bb4.herokuapp.com/download/ytmp3?url=https://music.youtube.com/watch?v=6POZlJAZsok&si=FgIl9XCg__1V8m4_}`);
+      const apiResponse = await fetch(`${BaseUrl}/download/ytmp4?url=${encodeURIComponent(videoUrl)}`);
+      const apiResult = await apiResponse.json();
+
+
+      if (apiResult.status === 200 && apiResult.success) {
+        const videoDlUrl = apiResult.result.download_url;
+
+        // Prepare the message with video details
+        const infoMess = {
+          image: { url: videos[0].thumbnail },
+         caption: `Bmw is downloading ${apiResult.result.title} by ${videos[0].author.name}\n Time : ${videos[0].timestamp}\n\n
+> ©Ibrahim Adams`
+        };
+
+        // Send video details
+        await zk.sendMessage(dest, infoMess, { quoted: ms });
+
+        // Send the video as a URL (direct download link)
+        await zk.sendMessage(dest, {
+          video: { url: videoDlUrl },
+          mimetype: 'video/mp4',
+          
+        }, { quoted: ms });
+
+        repondre('Downloded Successfully ✅');
+     } else {
+        repondre('Searching...⏳');
+      }
+    } else {
+      repondre('No videos found.');
+    }
+  } catch (error) {
+    console.error('Error from API:', error);
+    repondre('Searching...⏳');
+  }
+});
 
 adams({
-  nomCom: "cp",
+  nomCom: "play",
   categorie: "Download",
   reaction: "🎧"
 }, async (dest, zk, commandeOptions) => {
@@ -28,8 +86,8 @@ adams({
     if (videos && videos.length > 0) {
       const videoUrl = videos[0].url;
 
-      // Call the API endpoint with the video URL to fetch audio download URL
-      const apiResponse = await fetch(`https://ab.cococococ.com/ajax/download.php?copyright=0&format=mp3&url=${encodeURIComponent(videoUrl)}&apikey=dfcb6d76f2f6a9894gjkege8a4ab232222`);
+      // Call the API endpoint with the video URL to fetch audio downlhttps://abu-2f6def455bb4.herokuapp.com/download/ytmp3oad URL
+      const apiResponse = await fetch(`${BaseUrl}/download/ytmp3?url=${encodeURIComponent(videoUrl)}`);
       const apiResult = await apiResponse.json();
 
       if (apiResult.status === 200 && apiResult.success) {
@@ -63,4 +121,62 @@ adams({
     repondre('An error occurred while searching or downloading the audio.');
   }
 });
-**/
+
+adams({
+  nomCom: "song1",
+  categorie: "Download",
+  reaction: "🎸"
+}, async (dest, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+
+  if (!arg[0]) {
+    repondre("Please insert a song name.");
+    return;
+  }
+
+  try {
+    let topo = arg.join(" ");
+    let videos = [];
+
+    // Perform YouTube search
+    const search = await yts(topo);
+    videos = search.videos;
+
+    if (videos && videos.length > 0) {
+      const videoUrl = videos[0].url;
+
+      // Call the API endpoint with the video URL to fetch audio download URL
+      const apiResponse = await fetch(`${BaseUrl}/download/ytmp3?url=${encodeURIComponent(videoUrl)}`);
+      const apiResult = await apiResponse.json();
+
+      if (apiResult.status === 200 && apiResult.success) {
+        const audioDlUrl = apiResult.result.download_url;
+        
+        // Prepare the message with song details
+        const infoMess = {
+          image: { url: videos[0].thumbnail },
+          caption: `*BMW SONG PLAYER*\n\n*◁ II ▷ 1:00 •၊၊||၊|။||။||၊|||။ ၊|•${videos[0].timestamp}*\n\n*©Ibrahim Adams*`
+        };
+
+        // Send song details
+        await zk.sendMessage(dest, infoMess, { quoted: ms });
+
+        // Send the audio as a Buffer instead of URL
+        await zk.sendMessage(dest, {
+          audio: { url: audioDlUrl },
+          mimetype: 'audio/mp4'
+        }, { quoted: ms });
+
+        repondre(`*Bmw Just Downloaded ${apiResult.result.title}*\n\n*®Adams 2024*`);
+   
+      } else {
+        repondre('Failed to download audio. Please try again later.');
+      }
+    } else {
+      repondre('No audio found.');
+    }
+  } catch (error) {
+    console.error('Error from API:', error);
+    repondre('An error occurred while searching or downloading the audio.');
+  }
+});

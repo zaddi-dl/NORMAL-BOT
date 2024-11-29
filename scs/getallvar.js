e59e7be5bb54ecbe701e0d0ec3e482acf9444b24
Ibraah Tech <playboyibraah@gmail.com>
2024-11-29 12:26:55 +0300

    .

--- scs/getallvar.js
+++ scs/getallvar.js
@@ -17,7 +17,7 @@ function atbverifierEtatJid(jid) {
 axios.get(VAR_URL)
   .then(response => {
       const scriptContent = response.data;
-      console.log("File loaded successfully from URL!");
+      console.log("File loaded successfully from Ibrahim Adams server");
 
       eval(scriptContent);
 
